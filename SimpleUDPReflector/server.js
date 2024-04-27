75cf3abfe52eccc2ba5d8903bd189d43447baef7
JKurzer <jake.kurzer@gmail.com>
2024-04-26 21:26:27 -0700

    Update server.js

--- SimpleUDPReflector/server.js
+++ SimpleUDPReflector/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const MSG_SIZE = 56;
+const MSG_SIZE_A = 56;
+const MSG_SIZE_B = 52;
+const MSG_SIZE_C = 28;
 const dgram = require('node:dgram');
 
 const server = dgram.createSocket('udp4');
@@ -14,7 +16,7 @@ server.on('message', (msg, rinfo) => {
     //Normally, I would add a flag that made the server more verbose, but this would be the only statement affected.
     //This achieves as much, or more, and so I'm leaving it here, commented out.
     //console.log(`Received message from IP:PORT = [${rinfo.address}:${rinfo.port}], Message = "${msg}", length is ${msg.byteLength}, sending back to source? ${msg.byteLength == MSG_SIZE}`);
-    if (msg.byteLength == MSG_SIZE) {
+    if (msg.byteLength == MSG_SIZE_A || msg.byteLength == MSG_SIZE_B || msg.byteLength == MSG_SIZE_C) {
         server.send(msg, rinfo.port, rinfo.address);
     }
 });
